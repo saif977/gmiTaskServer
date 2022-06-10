bbb96bd1052b63df080320cc1064834a39122e27
saif977 <56349432+saif977@users.noreply.github.com>
2022-06-11 00:25:33 +0530

    Delete client directory

--- client/src/assets/data.ts
+++
@@ -1,7 +0,0 @@
-export const gender: string[] = ['male', 'female','others'];
-export const additionalCourses: object[] = [
-  { value: 'dataStructures', isSelected: true },
-  { value: 'operatingSystem', isSelected: false },
-  { value: 'networking', isSelected: false },
-  { value: 'systemDesign', isSelected: false },
-];
